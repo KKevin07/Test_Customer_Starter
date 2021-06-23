5111bcb3583c51f80c404ae9e688f6333238fca6
KKevin07 <wweiwenw@qq.com>
2021-06-23 17:39:00 +0800

    Initial commit

---
+++ src/main/java/com/example/test_customer_starter/TestCustomerStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_customer_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCustomerStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCustomerStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_customer_starter/TestCustomerStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_customer_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCustomerStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
